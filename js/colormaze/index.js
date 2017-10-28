4bb467c6e9e449827129141bc805804721e1aa69
Mehmet Bektas <mehmetbektas@gmail.com>
2017-10-28 13:50:58 +0200

    completed

--- js/colormaze/index.js
+++ js/colormaze/index.js
@@ -3,31 +3,17 @@ let column = 2;
 
 $(function () {
     $(document).keyup(function (event) {
-
         $(".selected").removeClass('selected');
 
-
         if (event.which == 37 && column > 1) {
-
             column--
-
         } else if (event.which == 38 && row > 1) {
-
             row--
-
         } else if (event.which == 39 && column < 3) {
-
             column++
-
         } else if (event.which == 40 && row < 3) {
-
             row++
-
-        } else {
-
-            column = column;
-            row = row;
-        }
+        } 
 
         $('#' + row + '_' + column).addClass('selected');
 
