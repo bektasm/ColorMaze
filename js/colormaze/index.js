2d0480b2ed4ad8b8af748605a3d18505d9c802cf
Mehmet Bektas <mehmetbektas@gmail.com>
2017-10-28 13:17:19 +0200

    solution with another method

--- js/colormaze/index.js
+++ js/colormaze/index.js
@@ -1,18 +1,35 @@
-$( function() {
-  $('.cell').click( function() {
-    $('.cell').removeClass('selected');
-    $(this).addClass("selected");
-  } );
-} );
+$(function () {
+    $(document).keyup(function (event) {
+        
+        $(".selected").removeClass('selected');
+
+
+        if (event.which == 37) {
+            
+            column--
+
+            
+
+        } else if (event.which == 38) {
+            
+            row--
 
 
 
+        } else if (event.which == 39) {
+            
+            column++
 
+        } else {
+            
+            row++
 
+        }
+   
+        $('#'+row+'_'+column).addClass('selected');
+        
+    });
+});
 
-
-
-
-
-
-
+let row = 2;
+let column = 2;
